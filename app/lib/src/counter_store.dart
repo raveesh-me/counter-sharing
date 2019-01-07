59a0665dd76c209cec7224a3eb24b05e5cec4dc0
Raveesh Agarwal <raveeshr503@gmail.com>
2019-01-07 12:46:50 +0530

    imports shared_preferences begins implementing sharedPreferencesCounterStore

--- app/lib/src/counter_store.dart
+++ app/lib/src/counter_store.dart
@@ -1,1 +1,16 @@
 //implements the abstract counter store class with shared_preferences
+import 'package:core/core.dart';
+
+class SPCounterStore implements CounterStore {
+  @override
+  Future<Map<String, int>> getCountMap() {
+    // TODO: implement getCountMap
+    return null;
+  }
+
+  @override
+  setCount(int count) {
+    // TODO: implement setCount
+    return null;
+  }
+}
