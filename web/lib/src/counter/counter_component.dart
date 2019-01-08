93a4a2155ee975b7c5c442fa6763376a24bd3414
Raveesh Agarwal <raveeshr503@gmail.com>
2019-01-08 20:13:16 +0530

    bootstraps web for setting up CounterStore

--- web/lib/src/counter/counter_component.dart
+++ web/lib/src/counter/counter_component.dart
@@ -1,5 +1,6 @@
 import 'package:core/core.dart' as counter;
 import 'package:angular/angular.dart';
+import 'package:web/src/web_counter_store.dart';
 
 
 @Component(
@@ -12,16 +13,18 @@ import 'package:angular/angular.dart';
 )
 class CounterComponent implements OnInit, OnDestroy {
 
-  counter.CounterBloc bloc = counter.CounterBloc();
+  counter.CounterBloc bloc;
   int count = 0;
 
   @override
-  void ngOnInit() {
+  void ngOnInit() async {
     // TODO: implement ngOnInit
+    bloc = await counter.CounterBloc.fromStore(WebCounterStore());
     bloc.countStream.listen((data) {
       count = data;
     });
   }
+
   @override
   void ngOnDestroy(){
     bloc.dispose();
---
+++ web/lib/src/web_counter_store.dart
@@ -0,0 +1,18 @@
+import 'dart:indexed_db';
+import 'dart:html';
+import 'package:core/core.dart';
+
+class WebCounterStore implements CounterStore {
+
+  @override
+  Future<Map<String, int>> getCountMap() {
+    // TODO: implement getCountMap
+    return null;
+  }
+
+  @override
+  setCount(int count) {
+    // TODO: implement setCount
+    return null;
+  }
+}
